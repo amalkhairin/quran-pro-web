9df2359962ff74697392554878a833382bda9cd6
amalkhairinn <amalkhairinn@gmail.com>
2024-08-28 11:11:56 +0700

    fix api for prod

--- src/api/jsonServerApi.js
+++ src/api/jsonServerApi.js
@@ -1,7 +1,8 @@
 import axios from 'axios';
 
 const jsonApi = axios.create({
-  baseURL: 'http://localhost:3001/',
+  // baseURL: 'http://localhost:3001/',
+  baseURL: 'https://quran-pro-web.vercel.app/',
   timeout: 5000,
 });
 

2daf38295b9f4045651dc60de593c8c0a44e7116
amalkhairinn <amalkhairinn@gmail.com>
2024-08-28 09:49:15 +0700

    fix display ayat

--- src/components/DisplayAyat.jsx
+++ src/components/DisplayAyat.jsx
@@ -3,41 +3,31 @@ import React, { useEffect, useState } from 'react';
 
 function DisplayAyat({ surahName, ayat, tafsir }) {
 
-    // const [displayedText, setDisplayedText] = useState('');
-    // const [index, setIndex] = useState(0);
-    // const speed = 100;
-
-    // useEffect(() => {
-    //     if (index < ayat.teksIndonesia.length) {
-    //         const timeout = setTimeout(() => {
-    //             setDisplayedText((prev) => prev + ayat.teksIndonesia[index]);
-    //             setIndex((prev) => prev + 1);
-    //         }, speed);
-
-    //         return () => clearTimeout(timeout);
-    //     }
-    // }, [index, ayat.teksIndonesia, speed]);
-
     return (
         <div className='sm:w-3/4 bg-[#3C3E45] shadow-lg p-5 rounded-xl relative flex flex-col justify-center gap-5'>
-            <div className='flex-row flex gap-2'>
-                <div className='w-2 h-2 rounded-full bg-[#d32e2e]'></div>
-                <div className='w-2 h-2 rounded-full bg-[#e6e339]'></div>
-                <div className='w-2 h-2 rounded-full bg-[#2cb94f]'></div>
+            <div className='flex-row flex justify-between items-center'>
+                <div className='flex flex-row gap-2'>
+                    <div className='w-2 h-2 rounded-full bg-[#d32e2e]'></div>
+                    <div className='w-2 h-2 rounded-full bg-[#e6e339]'></div>
+                    <div className='w-2 h-2 rounded-full bg-[#2cb94f]'></div>
+                </div>
+                <div className='text-gray-400'>Random Ayat</div>
             </div>
-            <div className='text-4xl font-bold text-center'>{surahName}</div>
-            <div className='bg-[#212429] px-4 py-4 rounded-lg text-center'>
+            {surahName && <div className='text-4xl font-bold text-center'>{surahName} ayat {ayat.nomorAyat}</div>}
+            {ayat && <div className='bg-[#212429] px-4 py-4 rounded-lg text-center'>
                 <div className='text-[#13A795]'>{ayat.teksArab}</div>
                 <div className='text-center text-tiny italic text-gray-400 mt-2'>{ayat.teksLatin}</div>
-            </div>
-            <div>
+            </div>}
+            {ayat && <div>
                 <div className='text-tiny text-gray-400 font-bold'>Artinya:</div>
-                <div className='text-gray-300'>{ayat.teksIndonesia}</div>
-            </div>
-            <div>
+                <div className='text-gray-300'>
+                    {ayat.teksIndonesia}
+                </div>
+            </div>}
+            {ayat && <div>
                 <div className='text-tiny text-gray-400 font-bold'>Tafsir:</div>
                 <div className='text-gray-300'>{tafsir.teks}</div>
-            </div>
+            </div>}
         </div>
     );
 }
--- src/pages/RandomAyat.jsx
+++ src/pages/RandomAyat.jsx
@@ -1,74 +1,53 @@
-import React, { useEffect, useState } from 'react';
+import React, { Component } from 'react';
 import jsonApi from '../api/jsonServerApi';
+import DisplayAyat from '../components/DisplayAyat';
 
-function RandomAyat() {
-    const [surahName, setSurahName] = useState('');
-    const [randomAyat, setRandomAyat] = useState({});
-    const [randomTafsirAyat, setRandomTafsirAyat] = useState({});
-    const [randomIndex, setRandomIndex] = useState(Math.round(Math.random() * 5));
+class RandomAyatClass extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            surahName: '',
+            randomAyat: {},
+            randomTafsirAyat: {},
+            randomIndex: Math.round(Math.random() * 5),
+        };
+    }
 
-    // const getRandomIndexNumber = Math.round(Math.random() * 114);
-
-    const getSurah = async (nomorSurah) => {
+    getSurah = async (nomorSurah) => {
         const { data } = await jsonApi.get(`surahs/${nomorSurah}`);
-        return data
-    }
+        return data;
+    };
 
-    const getRandomAyat = async () => {
-        setRandomIndex(Math.round(Math.random() * 5));
-        const surah = await getSurah(randomIndex);
-        console.log(surah.ayat.length);
+    getRandomAyat = async () => {
+        this.setState({ randomIndex: Math.round(Math.random() * 5) });
+        const surah = await this.getSurah(this.state.randomIndex);
         const tafsir = surah.tafsir;
+        let randomIndexAyat = Math.round(Math.random() * (surah.ayat.length - 1));
+        randomIndexAyat = Math.max(0, Math.min(randomIndexAyat, surah.ayat.length - 1));
 
-        let randomIndexAyat = Math.round(Math.random() * surah.ayat.length - 1);
-        if (randomIndexAyat < 0) {
-            randomIndexAyat = 0
-        }
-        if (randomIndexAyat > surah.ayat.length - 1) {
-            randomIndexAyat = surah.ayat.length - 1
-        }
+        this.setState({
+            surahName: surah.namaLatin,
+            randomAyat: surah.ayat[randomIndexAyat],
+            randomTafsirAyat: tafsir[randomIndexAyat]
+        });
+    };
 
-        setSurahName(surah.namaLatin);
-        setRandomAyat(surah.ayat[randomIndexAyat]);
-        setRandomTafsirAyat(tafsir[randomIndexAyat]);
-        console.log(randomIndexAyat);
-
-        console.log(surah.ayat[randomIndexAyat]);
-        console.log(surah.namaLatin)
-        console.log("tafsir:");
-
-        console.log(tafsir[randomIndexAyat])
-
+    componentDidMount() {
+        this.getRandomAyat();
     }
 
-    useEffect(() => {
-        getRandomAyat();
-    }, [])
-    return (
-        <div className='sm:w-3/4 bg-[#3C3E45] shadow-lg p-5 rounded-xl relative flex flex-col justify-center gap-5'>
-            <div className='flex-row flex gap-2'>
-                <div className='w-2 h-2 rounded-full bg-[#d32e2e]'></div>
-                <div className='w-2 h-2 rounded-full bg-[#e6e339]'></div>
-                <div className='w-2 h-2 rounded-full bg-[#2cb94f]'></div>
+    render() {
+        return (
+            <div className='flex flex-col items-center'>
+                <button onClick={this.getRandomAyat} className='px-5 py-2 bg-[#13A795] rounded-lg hover:bg-[#95f3e8] w-auto mb-5'>Generate</button>
+                <DisplayAyat 
+                    surahName={this.state.surahName} 
+                    ayat={this.state.randomAyat} 
+                    tafsir={this.state.randomTafsirAyat} 
+                />
             </div>
-            <div className='flex justify-center items-center'>
-                <button onClick={getRandomAyat} className='px-5 py-2 bg-[#13A795] rounded-lg hover:bg-[#95f3e8] w-auto'>Generate</button>
-            </div>
-            <div className='text-4xl font-bold text-center'>{surahName} ayat {randomAyat.nomorAyat}</div>
-            <div className='bg-[#212429] px-4 py-4 rounded-lg text-center'>
-                <div className='text-[#13A795] text-lg'>{randomAyat.teksArab}</div>
-                <div className='text-center text-tiny italic text-gray-400 mt-2'>{randomAyat.teksLatin}</div>
-            </div>
-            <div>
-                <div className='text-tiny text-gray-400 font-bold'>Artinya:</div>
-                <div className='text-gray-300'>{randomAyat.teksIndonesia}</div>
-            </div>
-            <div>
-                <div className='text-tiny text-gray-400 font-bold'>Tafsir:</div>
-                <div className='text-gray-300'>{randomTafsirAyat.teks}</div>
-            </div>
-        </div>
-    )
+        );
+    }
 }
 
-export default RandomAyat
+export default RandomAyatClass;
